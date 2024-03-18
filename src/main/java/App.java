6644bece20b10f7d557d39f89a3a24acf4ff1a6d
Vladimir <marychev21@gmail.com>
2024-03-18 22:59:36 +0300

    Add cat and scope(prototype)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hwBean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld hwBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+
+        System.out.println("helloworld-bean1 == helloworld-bean2: " + (hwBean1 == hwBean2));
+        System.out.println("cat-bean1 == cat-bean2: " + (cat1 == cat2));
     }
 }
